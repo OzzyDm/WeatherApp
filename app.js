9752d3d89ebdf980157c462750259d49321a7769
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-07-31 01:36:50 -0400

    show page updated

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 const { weather } = require("./public/javascript/search");
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 dotenv.config({ path: ".env" });
 const weatherAPI = process.env.weatherAPI;
@@ -16,17 +13,26 @@ const weatherAPI = process.env.weatherAPI;
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("show");
+app.get("/", async function (req, res) {
+  try {
+    const response = await weather("Toronto", weatherAPI);
+    res.render("show", { response });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/search", async function (req, res) {
   try {
-    const response = await weather("ankara", weatherAPI);
-    console.log(response.data.name);
+    const response = await weather("bingol", weatherAPI);
+    res.render("show", { response });
   } catch (error) {
     console.log(error);
   }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
