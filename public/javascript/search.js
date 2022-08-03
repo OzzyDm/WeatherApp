905635f8bf98971fd4377d92a2a94ab78c6253a3
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-08-03 02:22:15 -0400

    styles updated

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,17 +1,13 @@
 const axios = require("axios");
 
 async function weather(city, weatherAPI) {
-  if (city == undefined) {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPI}&units=metric`
+    );
+    return response;
+  } catch (error) {
     return "There is no such place!";
-  } else {
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPI}&units=metric`
-      );
-      return response;
-    } catch (error) {
-      return "There is no such place!";
-    }
   }
 }
 
