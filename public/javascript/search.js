3b8374716408d8ddbb8becd97e2e7b75c9180058
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-08-04 16:43:08 -0400

    search updated

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 async function weather(city, weatherAPI) {
-  if (city !== undefined || isNaN(city) === false) {
+  if (city !== undefined && !Number.isInteger(parseInt(city))) {
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPI}&units=metric`
     );
